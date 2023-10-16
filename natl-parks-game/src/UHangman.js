8e98e634a30701d617650024016277c8ceea2a59
Tiana Lopez <tianadavin@gmail.com>
2023-10-16 15:01:14 -0700

    Adds losing functionality

--- natl-parks-game/src/UHangman.js
+++ natl-parks-game/src/UHangman.js
@@ -4,12 +4,24 @@ import "./Hangman.css"
 function UHangman({parkObj}) {
   const [randomAttraction, setRandomAttraction] = useState("")
   const [correctGuesses, setCorrectGuesses] = useState([])
-  const [usedLetters, setUsedLetters] = useState([])
+  const [wrongGuesses, setWrongGuesses] = useState([])
+  const [status, setStatus] = useState("")
+
+  console.log(wrongGuesses)
+  console.log(wrongGuesses.length)
 
   //define letters
   const alphabets = ["A", "B", "C", "D", "E", "F", "G",
     "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
     "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  //reset game (with a loss)
+  const reset = () => {
+    randomElement(selectedPark)
+    setCorrectGuesses((resets) => [])
+    setWrongGuesses((resets) => [])
+    setStatus("")
+  }
 
   //temporary
   const selectedPark = parkObj[0].attractions
@@ -24,33 +36,58 @@ function UHangman({parkObj}) {
     console.log('useEffect triggered')
     randomElement(selectedPark)
   }, [selectedPark])
-  //reset display of hidden word
-  useEffect(() => {
-
-  })
 
  //handle click
  const handleClick = (letter) => {
-   if (randomAttraction.includes(letter) || !usedLetters.includes(letter)) {
+  if (wrongGuesses.length > 5 || status)
+    return
+  if (randomAttraction.includes(letter)) {
      setCorrectGuesses([...correctGuesses, letter])
-     setUsedLetters([...usedLetters, letter]);
-   }}
+   }
+   else {
+    setWrongGuesses([...wrongGuesses, letter])
+   }
+  }
 
   //render buttons filtering for already used buttons
   const renderButtons = () => {
     return alphabets
-      .filter((letter) => (!usedLetters.includes(letter)))
       .map((letter) => (
         <button className="letter"
           key={letter}
           onClick={() => handleClick(letter)}
-          disabled={usedLetters.includes(letter)}
+          disabled={correctGuesses.includes(letter) || wrongGuesses.includes(letter)}
         >
           {letter}
         </button>
       ))
   }
-
+//check if won
+  useEffect(() => {
+    if (correctGuesses.length && randomAttraction.split("").every(letter => correctGuesses.includes(letter)))
+      setStatus('win');
+  }, [correctGuesses])
+//check if lost
+  useEffect(() => {
+    if (wrongGuesses.length === 5)
+      setStatus('lose');
+  }, [wrongGuesses])
+//check the progress
+const progress = () => {
+  return <p>{`You have ${wrongGuesses.length}/5 wrong guesses`}</p>
+}
+//show end result
+const renderStatus = () => {
+  if (!status) {
+    return
+  }
+  else {
+    return <div>
+      <p>You {status}!</p>
+      <p>{status === "win" ? "Claim your Card" : "Try again"}</p>
+    </div>
+  }
+}
 
   //display hidden word
   const hiddenWord = randomAttraction.split('').map(letter =>
@@ -58,8 +95,9 @@ function UHangman({parkObj}) {
 
   return <div>
     <p>{hiddenWord}</p>
+    <p>{progress()}</p>
     {renderButtons()}
-    {!hiddenWord.includes("_") && <p>You won!</p>}
+    {renderStatus()}
   </div>
 }
 
