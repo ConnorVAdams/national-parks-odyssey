08ba6b8ffd8ff016c079c4077713a571df5134ca
Connor Adams <connorvadams@gmail.com>
2023-10-15 18:37:16 -0600

    Adds comments to memory game components.

--- natl-parks-game/src/AnimalBoard.js
+++ natl-parks-game/src/AnimalBoard.js
@@ -2,16 +2,21 @@ import { useState, useEffect } from 'react'
 import AnimalTile from './AnimalTile'
 
 const AnimalBoard = ({ animals, handleWin }) => {
+    //count variable keeps track of number of turns player has taken, with an odd numbered count being the middle of a turn and an even number being the end of a turn.
     const [count, setCount] = useState(0)
+    //clickedName variable holds the value of the first tile clicked every turn.
     const [clickedName, setClickedName] = useState('')
+    //shuffledDeck variable creates a randomized deck of tiles on initial render
     const [shuffledDeck, setShuffledDeck] = useState([])
 
+    //Recieves animal objects and duplicates the object with a new id.
     const duplicateCards = (arr) => {
         const duplicates = arr.map(card => ({ ...card, id: card.id + arr.length }))
         const combined = [...duplicates, ...arr]
         return combined
     }    
 
+    //Receives duplicated collection and randomly shuffles it.
     const shuffleDeck = (arr) => {
         for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -20,13 +25,18 @@ const AnimalBoard = ({ animals, handleWin }) => {
         return arr
     }
 
+    //Callback for onClick on animal tile.
     const handleSelectAnimal = (name) => {
+        //TODO Refactor first two ifs into a single if (count % 2 !== 0) statement?
+        //If it's end of turn, check most recently clicked name against value stored in clickedName.
         if (count % 2 !== 0 && name === clickedName){
+            //If it's a match, reset clickedName, find matching animal objects, change found property to true, and re-render.
             console.log('Good job!')
             setClickedName('')
             setCount(count => count + 1)
             setShuffledDeck(current => current.map(animal => animal.name === name ? { ...animal, found: true } : animal))
         } else if (count % 2 !== 0) {
+            //If it's not a match, give user 1.4 secs to digest both animals, but disable pointer events so user cannot reveal more tiles, then rest clickedName and re-render.
             document.querySelector('.animal-board').classList.add('disabled')
             setTimeout(() => {
                 setClickedName('')
@@ -34,26 +44,32 @@ const AnimalBoard = ({ animals, handleWin }) => {
                 }, 1400)
             console.log('Better luck next time.')
         } else {
+            //If it's mid-turn, store first clicked tile in clickedName and re-render.
             setClickedName(name)
             setCount(count => count + 1)
         }
+        //increment count by 1 in all cases.
     }
 
+    //Create unique deck for game on first render.
     useEffect(() => {
         setShuffledDeck(shuffleDeck(duplicateCards(animals)))
     }, [])
 
+    //Re-enable pointer events after every turn.
     useEffect(() => {
         document.querySelector('.animal-board').classList.remove('disabled')
     }, [count])
 
+    //Initiate handleWin() callback if all tiles in deck have been found.
     useEffect(() => {
         if ((shuffledDeck.filter(card => !card.found)).length === 0) {
             const endTime = Date.now()
             handleWin(endTime, count)
         }
     }, [shuffledDeck])
-
+    
+    //Render <AnimalTile /> components for re-use on the board.
     const animalDisplay = shuffledDeck.map(animal => {
         return <AnimalTile 
                     key={animal.id} 
@@ -66,7 +82,6 @@ const AnimalBoard = ({ animals, handleWin }) => {
 
     return (
     <div className={'animal-board'}>
-        <h2>{}</h2>
         {animalDisplay}
     </div>
     )
--- natl-parks-game/src/App.js
+++ natl-parks-game/src/App.js
@@ -5,12 +5,17 @@ import Hangman from "./Hangman"
 
 function App() {
 
+  //Score calculator specific to memory game
+  //TODO Can be generalized to calculate score for every game?
   const calculateScore = (finalTime, finalCount) => {
     const minimumMoves = 1
     const minimumTime = Date.now()
+    //Awards player more points for lower number of moves and/or lower elapsed time, with a maximum of 10,000.
+    //TODO What should maximum points for each game be?
     return Math.round((minimumTime / finalTime) * (minimumMoves / finalCount) * 10000)
   }
 
+  //Receives elapsed time and number of moves from <AnimalBoard /> and passes it through calculateScore()
   const handleWin = (endTime, count) => {
     const score = calculateScore(endTime, count)
     console.log(`Congratulations, you earned ${score} points!`)
@@ -18,6 +23,7 @@ function App() {
 
   return (
     <div className="App">
+        {/*<Game /> component for memory game.*/}
         <AnimalBoard animals={animals} handleWin={handleWin} />
         <Hangman />
     </div>
