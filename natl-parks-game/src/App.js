a4bc7e7808ffa2c1d74e5208184dd0d1e398e1c0
Connor Adams <connorvadams@gmail.com>
2023-10-15 19:17:25 -0600

    Initial commit.

--- natl-parks-game/src/App.js
+++ natl-parks-game/src/App.js
@@ -6,28 +6,30 @@ import Hangman from "./Hangman"
 
 function App() {
 
-  //Score calculator specific to memory game
-  //TODO Can be generalized to calculate score for every game?
-  const calculateScore = (finalTime, finalCount) => {
-    const minimumMoves = 1
-    const minimumTime = Date.now()
-    //Awards player more points for lower number of moves and/or lower elapsed time, with a maximum of 10,000.
-    //TODO What should maximum points for each game be?
-    return Math.round((minimumTime / finalTime) * (minimumMoves / finalCount) * 10000)
-  }
+  // //Score calculator specific to memory game
+  // //TODO Can be generalized to calculate score for every game?
+  // const calculateScore = (finalTime, finalCount) => {
+  //   const minimumMoves = 1
+  //   const minimumTime = Date.now()
+  //   //Awards player more points for lower number of moves and/or lower elapsed time, with a maximum of 10,000.
+  //   //TODO What should maximum points for each game be?
+  //   return Math.round((minimumTime / finalTime) * (minimumMoves / finalCount) * 10000)
+  // }
 
-  //Receives elapsed time and number of moves from <AnimalBoard /> and passes it through calculateScore()
-  const handleWin = (endTime, count) => {
-    const score = calculateScore(endTime, count)
-    console.log(`Congratulations, you earned ${score} points!`)
-  }
+  // //Receives elapsed time and number of moves from <AnimalBoard /> and passes it through calculateScore()
+  // const handleWin = (endTime, count) => {
+  //   const score = calculateScore(endTime, count)
+  //   console.log(`Congratulations, you earned ${score} points!`)
+  // }
+
+
 
   return (
     <div className="App">
         {/*<Game /> component for memory game.*/}
-        {/* <AnimalBoard animals={animals} handleWin={handleWin} />
-        <Hangman /> */}
-        <Book />
+        {/* <AnimalBoard animals={animals} handleWin={handleWin} /> */}
+        {/* <Hangman /> */}
+        {/* <Book /> */}
     </div>
   );
 }
