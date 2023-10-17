13f36ddd3957aa89452c37eb00279179e77ad776
Tiana Lopez <tianadavin@gmail.com>
2023-10-17 08:30:44 -0700

    Adds api key

--- natl-parks-game/src/GoogleMap.js
+++ natl-parks-game/src/GoogleMap.js
@@ -5,10 +5,16 @@ function GoogleMap() {
   const [map, setMap] = useState();
   const ref = useRef();
 
-  useEffect(() => {
-    setMap(new window.google.maps.Map(ref.current))
+//contains map object styling and view on initial load
+  const mapOptions = {
+    mapId: process.env.NEXT_PUBLIC_GOOGLE_MAP_ID,
+    center: { lat: 37.09, lng: 95.71 },
+    zoom: 10,
+    disableDefaultUI: true,
+  }
+ useEffect(() => {
+    setMap(new window.google.maps.Map(ref.current, mapOptions))
   },[])
-
   return (
     <div ref={ref} className='map' style={{ height: "100%", width: '100%' }}>
 
