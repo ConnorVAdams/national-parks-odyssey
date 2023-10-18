19c68db889228b45a6d600a5b4879d5d3888d347
Tiana Lopez <tianadavin@gmail.com>
2023-10-18 15:46:06 -0700

    updates letter css

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -3,41 +3,57 @@ import { Outlet, useOutletContext, useNavigate, useLocation } from "react-router
 
 
 const Game = () => {
-  const [currentPark, setCurrentPark] = useState({})
   const { handleWin } = useOutletContext()
   const navigate = useNavigate()
-
   const locationData = useLocation()
-  const { state: { id, path } } = locationData
+  const [currentGameData, setCurrentGameData] = useState({
+    park: {},
+    id: 0,
+    path: ''
+  })
 
-  const initiateGame = (id) => {
-    fetch(`http://localhost:3000/parkObj/${id}`)
-    .then(resp => resp.json())
-    .then(data => setCurrentPark(data))
-  }
+  useEffect(() => {
+    fetch(`http://localhost:3000/parkObj/${locationData.state.id}`)
+      .then(resp => resp.json())
+      .then(data => {
 
-  initiateGame(id)
-  console.log(id, path, currentPark)
+        setCurrentGameData(current => ({
+          ...current,
+          park: data,
+          id: locationData.state.id,
+          path: locationData.state.path
+        }))
+      })
+  }, [])
+
+  console.log(currentGameData)
 
   //User gets 3 minutes to play game before being navigated back to home
   setTimeout(() => {
     //TODO Display 'Sorry, you ran out of time!' message to user.
     navigate('/')
-  }, 180000000) //oldvalue was 180000
+  }, 18000000) //temporary change
 
+  //TODO Route protection
   // useEffect(() => {
   //   if (!id) {
   //     navigate('/')
-  //     //TODO select a park msg
+  // Alert user to 'select a park' msg
   //   }
   // })
 
-  const { location, wildlife, attractions } = currentPark
+  // const { location, wildlife, attractions } = currentPark
+
+  const { park: { attractions }, id, path } = currentGameData
 
   return (
     <div className="game">
-      <button className="return-home-button">X</button>
-      <Outlet context={{ handleWin, wildlife, location, attractions }}/>
+      <button onClick={() => navigate('/')} className="return-home-button">X</button>
+      <button onClick={() => navigate(path)} className="restart-game-button">Restart Game</button>
+      {/* Should we allow user to choose a different type of game without returning to Home? */}
+      {/* Destructure whatever park.property props you need for your game above and feed them to the context below:  */}
+      <Outlet context={{ handleWin, attractions }} />
+      {/* <NotifyBar /> */}
     </div>
   )
 }
