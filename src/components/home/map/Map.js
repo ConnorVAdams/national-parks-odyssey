21fb78987317df1db42c41890f02b830afd91738
Connor Adams <connorvadams@gmail.com>
2023-10-19 15:46:48 -0600

    delete comments.

--- src/components/home/map/Map.js
+++ src/components/home/map/Map.js
@@ -13,7 +13,7 @@ const Map = () => {
     //Provide three possible game paths.
     const games = ['/game/memory', '/game/hangman', '/game/arrow-game']
     const randomPath = games[Math.floor(Math.random() * games.length)]
-    navigate('/game/memory', { state: {id: parkID, path: '/game/memory'} })
+    navigate('/game/arrow-game', { state: {id: parkID, path: '/game/memory'} })
   }
 
   return (
