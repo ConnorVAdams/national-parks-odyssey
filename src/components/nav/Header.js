c5dfe5a9c3e078bfc1b7361bff36fae89aae5341
Tiana Lopez <tianadavin@gmail.com>
2023-10-19 13:54:51 -0700

    formatting header

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom"
 import React, { useState } from 'react'
 import Modal from "./UserModal"
+import "./Header.css"
+
 
 const Header = ({ currentUser, onLoginUserSubmit, onCreateUserSubmit }) => {
   const [isCreateUserModalOpen, setCreateUserModalOpen] = useState(false);
@@ -28,27 +30,29 @@ const Header = ({ currentUser, onLoginUserSubmit, onCreateUserSubmit }) => {
     <div className='header'>
       <div className='title-container'>
         <img onClick={() => navigate('./')} className='nps-logo' src='https://wikiwandv2-19431.kxcdn.com/_next/image?url=https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Logo_of_the_United_States_National_Park_Service.svg/640px-Logo_of_the_United_States_National_Park_Service.svg.png&w=640&q=50' alt='nps-logo' />
-        <h1 className='title'>Untitled National Parks Game</h1>
+        <h1 className='title'>National Parks Odyssey (TBD)</h1>
       </div>
       <div className='user-container'>
-      <button onClick={handleOpenCreateUserModal}>Create User</button>
-        <Modal
-          isOpen={isCreateUserModalOpen}
-          onClose={handleCloseCreateUserModal}
-          onUserSubmit={onCreateUserSubmit}
-        />
+        <div className='point-container'>
+          <h2>Current User: {currentUser.username} |</h2>
+          <h2> | {currentUser.points} Points</h2>
+        </div>
+        <button id="create-user" onClick={handleOpenCreateUserModal}>Create User</button>
+          <Modal
+            isOpen={isCreateUserModalOpen}
+            onClose={handleCloseCreateUserModal}
+            onUserSubmit={onCreateUserSubmit}
+          />
 
-        <button onClick={handleOpenLoginModal}>Login</button>
-        <Modal
-          isOpen={isLoginModalOpen}
-          onClose={handleCloseLoginModal}
-          onLoginUserSubmit={onLoginUserSubmit}
-        />
+          <button id="login" onClick={handleOpenLoginModal}>Login</button>
+          <Modal
+            isOpen={isLoginModalOpen}
+            onClose={handleCloseLoginModal}
+            onLoginUserSubmit={onLoginUserSubmit}
+          />
+
       </div>
-      <div className='point-container'>
-        <h2>Current User: {currentUser.username}</h2>
-        <h2> {currentUser.points} points</h2>
-      </div>
+
     </div>
   )
 }
