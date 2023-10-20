56c98ed2ddec1c02c7a34a13e55407e4449d8fa2
Tiana Lopez <tianadavin@gmail.com>
2023-10-20 00:45:14 -0700

    nav bar styling

--- src/components/nav/NavButtons.js
+++ src/components/nav/NavButtons.js
@@ -1,11 +1,27 @@
 import { NavLink } from 'react-router-dom'
+import "./NavButtons.css"
 
 const NavButtons = () => {
   return (
     <nav className='nav-buttons'>
-        <NavLink to='/' className='nav-link'>Home</NavLink>
-        <NavLink to='/book' className='nav-link'>Encyclopedia</NavLink>
-        <NavLink to='/rules' className='nav-link'>Rules</NavLink>
+      <NavLink to='/' className='nav-link'>
+        <div className="nav-link-item">
+          <img className="mountain" src="https://static.vecteezy.com/system/resources/thumbnails/001/206/260/small/mountain.png" alt="Home Icon" />
+          Home
+        </div>
+      </NavLink>
+      <NavLink to='/book' className='nav-link'>
+        <div className="nav-link-item">
+          <img className="mountain" src="https://static.vecteezy.com/system/resources/thumbnails/001/206/260/small/mountain.png" alt="Book Icon" />
+          Encyclopedia
+        </div>
+      </NavLink>
+      <NavLink to='/rules' className='nav-link'>
+        <div className="nav-link-item">
+          <img className="mountain" src="https://static.vecteezy.com/system/resources/thumbnails/001/206/260/small/mountain.png" alt="Rules Icon" />
+          Rules
+        </div>
+      </NavLink>
     </nav>
   )
 }
